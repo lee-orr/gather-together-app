fb80c4d41a3dc8bc9c616e36add390fadc15f7f3
lee-orr <lee-orr@users.noreply.github.com>
2023-02-18 13:55:56 -0500

    works with docker compose in dev!

--- sign-management-api/src/lib.rs
+++ sign-management-api/src/lib.rs
@@ -11,5 +11,5 @@ fn handle_sign_management_api(req: Request) -> Result<Response> {
     Ok(http::Response::builder()
         .status(200)
         .header("foo", "bar")
-        .body(Some("Hello, Fermyon".into()))?)
+        .body(Some("Hi!".into()))?)
 }
