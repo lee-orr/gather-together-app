df1041d0d2dfc92a1e45aa495e63be7a428016fe
lee-orr <lee-orr@users.noreply.github.com>
2023-02-25 23:01:14 -0500

    trying to get it working

--- messaging-backend/src/lib.rs
+++ messaging-backend/src/lib.rs
@@ -6,7 +6,6 @@ use std::str::from_utf8;
 /// A simple Spin Redis component.
 #[redis_component]
 fn on_message(message: Bytes) -> Result<()> {
-    println!("{}", from_utf8(&message)?);
-    // Implement me ...
+    println!("Hey {}", from_utf8(&message)?);
     Ok(())
 }
